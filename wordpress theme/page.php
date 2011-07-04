eca7b2eebad2a726e278c670e67be41973ef2d8c
Aaron Neville <aaron@designliftoff.com>
2011-07-04 09:20:31 -0700

    added wordpress theme

---
+++ wordpress theme/page.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Standard Page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the wordpress construct of pages
+ * and that other 'pages' on your wordpress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers 3.0
+ */
+
+get_header(); ?>
+
+
+<section id="body_wrapper">
+
+<section id="content">
+
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+					<?php if ( is_front_page() ) { ?>
+						<h2><?php the_title(); ?></h2>
+					<?php } else { ?>	
+						<h1><?php the_title(); ?></h1>
+					<?php } ?>				
+
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'twentyten' ), '', '' ); ?>
+
+				<?php comments_template( '', true ); ?>
+
+</section>  <!-- end content -->
+
+<?php endwhile; ?>
+
+<aside id="sidebar">
+<?php get_sidebar(); ?>
+</aside>
+
+</section>
+
+<?php get_footer(); ?>
